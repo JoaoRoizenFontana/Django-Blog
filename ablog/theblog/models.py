2c20444a4cbf5d0329bb1541f72e93bbb96cb326
Font <[200~38985296+JoaoRoizenFontana@users.noreply.github.com~>
2023-10-07 20:21:45 -0300

    comments functionality

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -14,3 +14,10 @@ class Post(models.Model):
         #return reverse('article-detail', args=(str(self.id)) )
         return reverse('home')
     
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments" , on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def  __str__(self):
+        return (self.post.title, self.name)
