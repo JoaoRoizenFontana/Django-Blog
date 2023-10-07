afb1ab644d93a26e0c2923eca282c8168b636539
Font <[200~38985296+JoaoRoizenFontana@users.noreply.github.com~>
2023-10-07 17:49:28 -0300

    user user authentication

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -13,3 +13,16 @@ class PostForm(forms.ModelForm):
 
 
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title',  'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+
+
+        }
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from theblog.models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 #def home(request):
 #   return render(request, 'home.html', {})
@@ -9,6 +10,7 @@ from .forms import PostForm
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    #ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -23,5 +25,11 @@ class AddPostView(CreateView):
 
 class UpdatePostView(UpdateView):
     model = Post
+    form_class = EditForm
     template_name = 'update_post.html'
-    fields = ['title', 'body']
+    #fields = ['title', 'body']
+    
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
