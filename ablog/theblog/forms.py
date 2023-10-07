4a6e29f070ca068ee51d8f08eec4a5662bb5e4f2
Font <[200~38985296+JoaoRoizenFontana@users.noreply.github.com~>
2023-10-07 18:39:58 -0300

    users restrictions

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -8,7 +8,8 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
 
 
