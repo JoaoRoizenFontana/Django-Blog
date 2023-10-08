0249a68efeeda36b3b5942ab2bf57ce84ae7484d
Font <[200~38985296+JoaoRoizenFontana@users.noreply.github.com~>
2023-10-07 23:48:34 -0300

    vercel deploy

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1lfjww9rr@#+y@0gvbivfdr6)@)nc)mnwnbok7p5w*78jg=x@-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
